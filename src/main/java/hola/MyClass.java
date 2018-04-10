c5c49e2acc73e0b20189cf5536c1a755dbf36a8e
X78482CA <david.castrojimenez@gexterno.es>
2018-04-10 09:58:27 +0200

    Tests added

---
+++ src/main/java/hola/MyClass.java
@@ -0,0 +1,9 @@
+package hola;
+
+public class MyClass {
+
+	public int multiply(int a, int b) {
+		return a*b;
+	}
+	
+}
---
+++ src/test/java/tests/Testing.java
@@ -0,0 +1,22 @@
+package tests;
+
+import org.junit.Assert;
+import org.junit.Test;
+import hola.MyClass;
+
+public class Testing {
+
+
+	@Test
+	public void multiplicationOfZeroIntegersShouldReturnZero() {
+		MyClass tester = new MyClass(); // MyClass is tested
+
+				
+		// assert statements
+		Assert.assertEquals(0, tester.multiply(10, 0));
+		Assert.assertEquals(0, tester.multiply(0, 10));
+		Assert.assertEquals(0, tester.multiply(0, 0));
+	}
+
+
+}
